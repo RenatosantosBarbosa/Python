5e69e7df3294f3af24f25e459762cf71dd75096a
Renato Dos santos Barbosa <renato.santos.barbosa@escola.pr.gov.br>
2025-10-11 22:25:06 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+nome = str(input("Qual seu nome?: "))
 idade = int(input("Digite sua idade: "))
 
 if idade < 18:
-    print("Menor de idade")
+    print(f"Olá {nome} vc é menor de idade") 
 else:
-    print("Maior de idade")
+    print(f"Olá {nome} vc é maior de idade")
